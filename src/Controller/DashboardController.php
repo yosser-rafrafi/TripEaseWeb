1e110540ad77e8120f44914322973002b95aa90e
Yasminelamri <yasmin.elamri@esprit.tn>
2025-04-16 00:33:15 +0200

    modification user

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -9,10 +9,44 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DashboardController extends BaseController
 {
-    #[Route('/', name: 'dashboard')]
+    #[Route('/dashboard', name: 'dashboard')]
     public function index(): Response
     {
-        // Redirige vers la page d'index des voyages (app_voyage_index)
-        return $this->redirectToRoute('app_voyage_index');
+        return $this->render('back/dashboard/index.html.twig', [
+            'stats' => [
+                'visitors' => '10k',
+                'volume' => '100%',
+                'files' => '2000+',
+                'mails' => '120',
+                'ratings' => '4000+',
+                'shares' => '1000',
+                'network' => '600',
+                'achievements' => '17'
+            ],
+            'support' => [
+                'total' => '350',
+                'open' => '10',
+                'running' => '5',
+                'solved' => '3'
+            ],
+            'projects' => [
+                // Project data
+            ],
+            'updates' => [
+                // Updates data
+            ],
+            'reviews' => [
+                // Reviews data
+            ],
+            'messages' => [
+                // Messages data
+            ]
+        ]);
+    }
+
+    #[Route('/back/form/elements', name: 'back_form_elements')]
+    public function elements(): Response
+    {
+        return $this->render('back/form/elements.html.twig');
     }
 }
