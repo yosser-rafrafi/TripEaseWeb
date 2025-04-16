63e8ad8fd1aec2aab0a8a44fb4e478b2494d6ef6
khalil <khalil.damak@esprit.tn>
2025-04-16 01:48:33 +0100

    CRUD Avance done

---
+++ src/Form/ManagerTraitementAvanceType.php
@@ -0,0 +1,36 @@
+<?php
+
+// src/Form/ManagerTraitementAvanceType.php
+
+namespace App\Form;
+
+use App\Entity\AvanceFrai;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ManagerTraitementAvanceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('montant_accorde', MoneyType::class, [
+                'label' => 'Montant accordé',
+                
+            ])
+            ->add('commentaire_manager', TextareaType::class, [
+                'label' => 'Commentaire du manager',
+                'required' => false,
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => AvanceFrai::class,
+        ]);
+    }
+}
+
