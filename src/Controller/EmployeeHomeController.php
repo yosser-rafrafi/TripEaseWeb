a61c44d8edd6090a0317f742fc3f3b4eed3d1656
yosser-rafrafi <yosserafrafi@gmail.com>
2025-04-16 23:23:10 +0100

    fixing errors

--- src/Controller/EmployeeHomeController.php
+++ src/Controller/EmployeeHomeController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use App\Repository\VoyageRepository;
+use App\Entity\Voyage;
+use App\Entity\Mission;
+use App\Controller\MissionController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EmployeeHomeController extends AbstractController
@@ -21,14 +25,33 @@ class EmployeeHomeController extends AbstractController
     }
 
     #[Route('/employee/my-travels', name: 'app_my_travels')]
-    public function myTravels(): Response
+    public function myTravels(VoyageRepository $voyageRepository): Response
     {
-        return $this->render('front/employee_home/my_travels.html.twig');
+
+        $user = $this->getUser();
+
+        $voyages = $voyageRepository->findVoyagesByUser($user);
+     
+         return $this->render('front/Voyage/my_travels.html.twig', [
+             'voyages' => $voyages,
+         ]);
+       
+    }
+
+    #[Route('/show/{id}', name: 'app_voyage_show_employee', methods: ['GET'])]
+    public function show(Voyage $voyage): Response
+    {
+        return $this->render('/front/Voyage/show.html.twig', [
+            'voyage' => $voyage,
+        ]);
     }
 
     #[Route('/employee/expenses', name: 'app_expenses')]
     public function expenses(): Response
     {
-        return $this->render('front/employee_home/expenses.html.twig');
+        return $this->redirectToRoute('app_avance_frai_index');
+       // return $this->render('front/employee_home/expenses.html.twig');
     }
+
+    
 }
